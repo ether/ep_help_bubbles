922a2dff9f71f04499bdb496fdebabeef1a96479
John McLear <john@mclear.co.uk>
2021-02-05 13:39:41 +0000

    lint

--- hooks.js
+++ hooks.js
@@ -1,12 +1,13 @@
-const path = require('path');
+'use strict';
+
 const eejs = require('ep_etherpad-lite/node/eejs');
 
-exports.eejsBlock_embedPopup = function (hook_name, args, cb) {
+exports.eejsBlock_embedPopup = (hookName, args, cb) => {
   args.content += eejs.require('ep_help_bubbles/templates/embedButtons.html');
-  return cb();
+  cb();
 };
 
-exports.eejsBlock_scripts = function (hook_name, args, cb) {
+exports.eejsBlock_scripts = (hookName, args, cb) => {
   args.content += eejs.require('ep_help_bubbles/templates/scripts.html', {}, module);
-  return cb();
+  cb();
 };
--- static/js/help_bubbles.js
+++ static/js/help_bubbles.js
@@ -1,13 +1,23 @@
-exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  args.content += require('ep_etherpad-lite/node/eejs/').require('ep_help_bubbles/static/js/Bubble.js');
-  args.content += require('ep_etherpad-lite/node/eejs/').require('ep_help_bubbles/static/js/jquery.grumble.js');
-  args.content += require('ep_etherpad-lite/node/eejs/').require('ep_help_bubbles/static/js/help_bubbles_function.js');
+'use strict';
+
+exports.eejsBlock_scripts = (hookName, args, cb) => {
+  args.content += require('ep_etherpad-lite/node/eejs/')
+      .require('ep_help_bubbles/static/js/Bubble.js');
+  args.content += require('ep_etherpad-lite/node/eejs/')
+      .require('ep_help_bubbles/static/js/jquery.grumble.js');
+  args.content += require('ep_etherpad-lite/node/eejs/')
+      .require('ep_help_bubbles/static/js/help_bubbles_function.js');
+  cb();
 };
 
-exports.eejsBlock_styles = function (hook_name, args, cb) {
-  args.content += require('ep_etherpad-lite/node/eejs/').require('ep_help_bubbles/static/css/help_bubbles.css');
+exports.eejsBlock_styles = (hookName, args, cb) => {
+  args.content += require('ep_etherpad-lite/node/eejs/')
+      .require('ep_help_bubbles/static/css/help_bubbles.css');
+  cb();
 };
 
-exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
-  args.content = require('ep_etherpad-lite/node/eejs/').require('ep_help_bubbles/templates/help_bubbles.ejs') + args.content;
+exports.eejsBlock_editbarMenuRight = (hookName, args, cb) => {
+  args.content = require('ep_etherpad-lite/node/eejs/')
+      .require('ep_help_bubbles/templates/help_bubbles.ejs') + args.content;
+  cb();
 };
