b25fb9432bcd80e1ccba3a07b19913c3d971f543
SamTV12345 <40429738+SamTV12345@users.noreply.github.com>
2026-05-25 16:54:32 +0200

    fix: backward-compat for ESM etherpad (#89)
    - Drop trailing slash on ep_etherpad-lite/node/eejs/ require
    
    Backward-compatible with current CJS etherpad release; also
    compatible with the upcoming ESM etherpad branch which has stricter
    exports map resolution.

--- static/js/help_bubbles.js
+++ static/js/help_bubbles.js
@@ -1,23 +1,23 @@
 'use strict';
 
 exports.eejsBlock_scripts = (hookName, args, cb) => {
-  args.content += require('ep_etherpad-lite/node/eejs/')
+  args.content += require('ep_etherpad-lite/node/eejs')
       .require('ep_help_bubbles/static/js/Bubble.js');
-  args.content += require('ep_etherpad-lite/node/eejs/')
+  args.content += require('ep_etherpad-lite/node/eejs')
       .require('ep_help_bubbles/static/js/jquery.grumble.js');
-  args.content += require('ep_etherpad-lite/node/eejs/')
+  args.content += require('ep_etherpad-lite/node/eejs')
       .require('ep_help_bubbles/static/js/help_bubbles_function.js');
   cb();
 };
 
 exports.eejsBlock_styles = (hookName, args, cb) => {
-  args.content += require('ep_etherpad-lite/node/eejs/')
+  args.content += require('ep_etherpad-lite/node/eejs')
       .require('ep_help_bubbles/static/css/help_bubbles.css');
   cb();
 };
 
 exports.eejsBlock_editbarMenuRight = (hookName, args, cb) => {
-  args.content = require('ep_etherpad-lite/node/eejs/')
+  args.content = require('ep_etherpad-lite/node/eejs')
       .require('ep_help_bubbles/templates/help_bubbles.ejs') + args.content;
   cb();
 };
